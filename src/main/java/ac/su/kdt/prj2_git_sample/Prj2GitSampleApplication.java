fee1400fdf0726f6a8b9f4610fd8cadefa006d3d
wild-mental <wild.mental@kidsnote.com>
2024-07-02 14:00:43 +0900

    initial commit

---
+++ src/main/java/ac/su/kdt/prj2_git_sample/Prj2GitSampleApplication.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.prj2_git_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2GitSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2GitSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/kdt/prj2_git_sample/Prj2GitSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.prj2_git_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2GitSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
